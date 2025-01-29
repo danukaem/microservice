42959f2b571cc90688393be4b5b8fb0fd846daf9
danukaem <danukaem@gmail.com>
2025-01-29 15:09:04 +0530

    circuit breaker added

--- inventory/src/main/java/com/inventory/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/inventory/inventory/controller/InventoryController.java
@@ -1,6 +1,8 @@
 package com.inventory.inventory.controller;
 
 import com.inventory.inventory.model.Order;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Slf4j
 @RestController
 @RequestMapping("inventory")
 public class InventoryController {
@@ -16,7 +19,9 @@ public class InventoryController {
     private WebClient.Builder webClientBuilder;
 
     @GetMapping("/getInventory")
+    @CircuitBreaker(name = "order", fallbackMethod = "getOrderFallback")
     public String getInventory() {
+        log.info("--------------------------------------------------getInventory method called--------------------------------------------------");
         String block = webClientBuilder.build().get().uri("http://api-gateway/product/getProducts")
                 .retrieve()
                 .bodyToMono(String.class)
@@ -43,6 +48,14 @@ public class InventoryController {
 
         System.out.println("block1 call order response: " + block1);
         return "Inventory : " + block;
-//        return "Inventory :*********** " ;
+    }
+
+    public String getOrderFallback(Exception e) {
+        return "Order Service is down. Please try again later";
+    }
+
+    @GetMapping("/getInventory2")
+    public String getInventory2(){
+        return "Inventory2";
     }
 }
